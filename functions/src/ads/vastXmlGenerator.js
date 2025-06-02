35dde6f528602c600493c53c28ddc986359efbb1
Kyunghwan Kim <kyunghwan.kim@motov.co.kr>
2025-06-02 10:46:46 +0900

    feat: VAST 4.0 XML 광고 생성기 구현
    - VAST 4.0 표준을 준수하는 XML 생성 로직 구현
    - 광고 이벤트 트래킹 기능 추가 (impression, start, complete 등)
    - 다중 미디어 파일 지원 (WebM, MP4)
    - UniversalAdId 및 InteractiveCreativeFile 등 VAST 4.0 필수 요소 포함
    - 트래킹 URL 파라미터 자동 생성 기능 구현

--- functions/src/ads/vastXmlGenerator.js
+++ functions/src/ads/vastXmlGenerator.js
@@ -1,21 +1,33 @@
 const { create } = require('xmlbuilder2');
 
 /**
- * 기본 VAST 3.0 XML을 생성합니다.
+ * VAST 4.0 XML을 생성합니다.
  * @param {string} [trackingUrl=''] - 이벤트 트래킹 기본 URL
  * @param {string} [adId='test-ad-1'] - 광고 ID
  * @return {string} VAST XML 문자열
  */
 const generateBasicVastXml = (trackingUrl = '', adId = 'test-ad-1') => {
   const vast = create({ version: '1.0', encoding: 'UTF-8' })
-    .ele('VAST', { version: '3.0' })
+    .ele('VAST', { version: '4.0' })
     .ele('Ad', { id: adId })
     .ele('InLine')
-    .ele('AdSystem')
+    .ele('AdSystem', { version: '1.0' })
     .txt('Test Ad Server')
     .up()
     .ele('AdTitle')
     .txt('Test Video Ad')
+    .up()
+    .ele('Description')
+    .dat('A test video ad')
+    .up()
+    .ele('Advertiser')
+    .txt('Test Advertiser')
+    .up()
+    .ele('Pricing', { model: 'CPM', currency: 'USD' })
+    .txt('0.0')
+    .up()
+    .ele('Error')
+    .dat(trackingUrl ? `${trackingUrl}?event=error[ERRORCODE]&adId=${adId}` : '')
     .up();
 
   // Impression 트래킹
@@ -24,84 +36,123 @@ const generateBasicVastXml = (trackingUrl = '', adId = 'test-ad-1') => {
     impressionUrl.searchParams.set('event', 'impression');
     impressionUrl.searchParams.set('adId', adId);
 
-    vast.ele('Impression')
+    vast.ele('Impression', { id: 'impression-1' })
       .dat(impressionUrl.toString())
       .up();
   }
 
-  // Linear 요소 생성
-  const linear = vast
-    .ele('Creatives')
-    .ele('Creative')
-    .ele('Linear');
-
-  // Duration 추가
-  linear.ele('Duration')
+  // Creatives 요소 생성
+  const creatives = vast.ele('Creatives');
+  const creative = creatives
+    .ele('Creative', { id: `creative-${adId}`, sequence: '1' })
+    .ele('Linear')
+    .ele('Duration')
     .txt('00:01:00')
+    .up();
+
+  // UniversalAdId 추가 (VAST 4.0의 새로운 필수 요소)
+  creative.ele('UniversalAdId', { idRegistry: 'motovvideo', idValue: 'unknown' })
+    .txt(adId)
     .up();
 
   // 트래킹 이벤트 처리
   if (trackingUrl) {
     const trackingEvents = [
-      'start',
-      'firstQuartile',
-      'midpoint',
-      'thirdQuartile',
-      'complete',
+      { event: 'start', offset: '00:00:00' },
+      { event: 'firstQuartile', offset: '00:00:15' },
+      { event: 'midpoint', offset: '00:00:30' },
+      { event: 'thirdQuartile', offset: '00:00:45' },
+      { event: 'complete', offset: '00:01:00' },
+      { event: 'progress', offset: '00:00:05' },
+      { event: 'mute' },
+      { event: 'unmute' },
+      { event: 'pause' },
+      { event: 'resume' },
+      { event: 'fullscreen' },
+      { event: 'exitFullscreen' },
+      { event: 'skip' },
     ];
 
-    const trackingElement = linear.ele('TrackingEvents');
+    const trackingElement = creative.ele('TrackingEvents');
 
-    trackingEvents.forEach((event) => {
+    trackingEvents.forEach(({ event, offset }) => {
       const url = new URL(trackingUrl);
       url.searchParams.set('event', event);
       url.searchParams.set('adId', adId);
 
-      trackingElement
-        .ele('Tracking', { event })
-        .dat(url.toString())
-        .up();
+      const tracking = trackingElement.ele('Tracking', { event });
+      if (offset) {
+        tracking.att('offset', offset);
+      }
+      tracking.dat(url.toString()).up();
     });
 
     trackingElement.up();
 
     // VideoClicks 추가
-    const clickUrl = new URL(trackingUrl);
-    clickUrl.searchParams.set('event', 'videoClick');
-    clickUrl.searchParams.set('adId', adId);
+    const videoClicks = creative.ele('VideoClicks');
 
-    linear.ele('VideoClicks')
-      .ele('ClickThrough')
+    // ClickThrough
+    const clickUrl = new URL(trackingUrl);
+    clickUrl.searchParams.set('event', 'clickThrough');
+    clickUrl.searchParams.set('adId', adId);
+    videoClicks.ele('ClickThrough', { id: 'click-1' })
       .dat(clickUrl.toString())
-      .up()
       .up();
+
+    // ClickTracking
+    const clickTrackingUrl = new URL(trackingUrl);
+    clickTrackingUrl.searchParams.set('event', 'click');
+    clickTrackingUrl.searchParams.set('adId', adId);
+    videoClicks.ele('ClickTracking', { id: 'clickTracking-1' })
+      .dat(clickTrackingUrl.toString())
+      .up();
+
+    videoClicks.up();
   }
 
-  // MediaFiles 추가
-  // linear.ele('MediaFiles')
-  //   .ele('MediaFile', {
-  //     delivery: 'streaming',
-  //     type: 'video/webm',
-  //     bitrate: '454',
-  //     width: '1920',
-  //     height: '540',
-  //   })
-  //   // .dat('https://redirector.gvt1.com/videoplayback/id/e03cfa4e6f65da6b/itag/18/source/dclk_video_ads/acao/yes/cpn/77BNTLkybJuB0Gw8/ctier/L/ei/OHotaOrvK-SP1d8Ptb3nqAg/ip/0.0.0.0/requiressl/yes/rqh/1/susc/dvc/xpc/Eghovf3BOnoBAQ%3D%3D/expire/1779346872/sparams/expire,ei,ip,requiressl,acao,ctier,source,id,itag,rqh,susc,xpc/sig/AJfQdSswRgIhAImARLCwFSYKsjBoA-nwZf6M3FYnzwEWJK1aiiRDVwB3AiEA-8z-4t7YREQQOGFkUAHuvnW-iLNzRpCWS0ijCHgDV5E%3D/file/file.mp4')
-  //   .dat('https://firebasestorage.googleapis.com/v0/b/vast-test-697cc.firebasestorage.app/o/sample_ads_webm%2F31047666.webm?alt=media&token=6e7a26a4-cf3c-4d1a-8943-c9a42f399538')
-  //   .up()
-  //   .up();
-
-  linear.ele('MediaFiles')
-    .ele('MediaFile', {
-      delivery: 'progressive',
-      type: 'video/mp4',
-      bitrate: '96',
-      width: '1280',
-      height: '360',
-    })
-    .dat('https://firebasestorage.googleapis.com/v0/b/vast-test-697cc.firebasestorage.app/o/sample_ads_webm%2F821294609%20(video-converter.com).mp4?alt=media&token=fc673ede-a04e-4fe0-bc27-0285b94361e9')
-    .up()
+  // MediaFiles 추가 (VAST 4.0에서는 mimes 속성이 필수)
+  const mediaFiles = creative.ele('MediaFiles');
+  mediaFiles.ele('MediaFile', {
+    id: 'video-1',
+    delivery: 'progressive',
+    type: 'video/webm',
+    bitrate: '454',
+    width: '1920',
+    height: '540',
+    scalable: 'true',
+    maintainAspectRatio: 'true',
+    codec: 'vp8',
+    apiFramework: 'VPAID 2.0',
+    mimes: 'video/webm',
+  })
+    .dat('https://firebasestorage.googleapis.com/v0/b/vast-test-697cc.firebasestorage.app/o/sample_ads_webm%2F31047666.webm?alt=media&token=6e7a26a4-cf3c-4d1a-8943-c9a42f399538')
     .up();
+  mediaFiles.ele('MediaFile', {
+    id: 'video-1',
+    delivery: 'progressive',
+    type: 'video/mp4',
+    bitrate: '96',
+    width: '1280',
+    height: '360',
+    scalable: 'true',
+    maintainAspectRatio: 'true',
+    codec: 'vp8',
+    apiFramework: 'VPAID 2.0',
+    mimes: 'video/mp4',
+  })
+    .dat('https://firebasestorage.googleapis.com/v0/b/vast-test-697cc.firebasestorage.app/o/sample_ads_webm%2F821294609%20(video-converter.com).mp4?alt=media&token=fc673ede-a04e-4fe0-bc27-0285b94361e9')
+    .up();
+  // InteractiveCreativeFile 추가 (VAST 4.0의 새로운 기능)
+  mediaFiles.ele('InteractiveCreativeFile', {
+    type: 'application/javascript',
+    apiFramework: 'VPAID',
+  })
+    .dat('')
+    .up();
+
+  mediaFiles.up();
+
   // 최종 XML 생성
   return vast.end({ prettyPrint: true });
 };
